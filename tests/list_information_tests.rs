45b5234937b35a1fcdcb99eeb433d2873481e1fb
Sam Schlinkert <sschlinkert@gmail.com>
2021-11-28 19:51:56 -0500

    adds a simple test of the assume entropy per letter function

--- tests/list_information_tests.rs
+++ tests/list_information_tests.rs
@@ -2,9 +2,18 @@ mod list_information_tests {
     use tidy::*;
 
     #[test]
-    fn can_calculate_entropy_of_finished_list() {
+    fn can_calculate_entropy_per_word_of_generated_list() {
         assert_eq!(calc_entropy(7_776), 12.92481250360578);
         assert_eq!(calc_entropy(16_103), 13.97504186800953);
         assert_eq!(calc_entropy(18_318), 14.160974374927937);
+    }
+
+    #[test]
+    fn can_calculate_assumed_entropy_per_letter_of_generated_list() {
+        let list: Vec<String> = vec!["to", "canopy", "cold", "seasons", "fire", "Christmas"]
+            .iter()
+            .map(|x| x.to_string())
+            .collect();
+        assert_eq!(assumed_entropy_per_letter(&list), 1.292481250360578);
     }
 }
