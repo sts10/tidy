2139c638c8a6adf9ad1dbb06b8564f8e61515256
Sam Schlinkert <sschlinkert@gmail.com>
2022-08-11 11:50:31 -0400

    adds test of a fixed length code

--- tests/uniquely_decodable_tests.rs
+++ tests/uniquely_decodable_tests.rs
@@ -1,16 +1,13 @@
 mod uniquely_decodable_tests {
     use tidy::display_information::uniquely_decodable::check_decodability;
 
-    fn make_list() -> Vec<String> {
-        vec!["news", "newspaper", "paper", "elephant"]
-            .iter()
-            .map(|x| x.to_string())
-            .collect()
-    }
-
     #[test]
     fn can_determine_a_list_with_prefix_words_is_not_uniquely_decodable() {
-        let list = make_list();
+        let list: Vec<String> = vec!["news", "newspaper", "paper", "elephant"]
+            .iter()
+            .map(|x| x.to_string())
+            .collect();
+
         assert!(!check_decodability(&list));
 
         let list2: Vec<String> = vec![
@@ -52,5 +49,16 @@ mod uniquely_decodable_tests {
             .map(|w| w.to_string())
             .collect();
         assert!(!check_decodability(&list));
+    }
+
+    #[test]
+    fn knows_that_a_fixed_length_code_is_uniquely_decodable() {
+        let list: Vec<String> = vec![
+            "buoy", "cote", "dads", "duel", "gale", "life", "lurk", "peer", "rain", "tong",
+        ]
+        .iter()
+        .map(|w| w.to_string())
+        .collect();
+        assert!(check_decodability(&list));
     }
 }
