88f0ca046efeb60a81f8bb6982b27a55efcc1330
Sam Schlinkert <sschlinkert@gmail.com>
2023-02-21 09:53:56 -0500

    changes Aces from 01 to Ac and, somewhat arbitrarily, makes them high rather than low

--- src/cards.rs
+++ src/cards.rs
@@ -1,5 +1,6 @@
 use radix_fmt::*; // https://stackoverflow.com/a/50278316
 
+/// Convert a number in a "card code" (base 26)
 pub fn print_as_cards(n: usize, list_length: usize) -> String {
     let n_as_base_26 = radix(n, 26);
 
@@ -25,35 +26,35 @@ pub fn print_as_cards(n: usize, list_length: usize) -> String {
         .to_string()
 }
 
-/// Convert 0-z inputted character to a 1-indexed, padded string ("01" to "36")
+/// Convert 0-z inputted character to a 3-character "card code"
 fn char_to_card(ch: char) -> String {
     match ch {
-        '0' => "B01",
-        '1' => "B02",
-        '2' => "B03",
-        '3' => "B04",
-        '4' => "B05",
-        '5' => "B06",
-        '6' => "B07",
-        '7' => "B08",
-        '8' => "B09",
-        '9' => "B10",
-        'a' => "BJa",
-        'b' => "BQu",
-        'c' => "BKi",
-        'd' => "R01",
-        'e' => "R02",
-        'f' => "R03",
-        'g' => "R04",
-        'h' => "R05",
-        'i' => "R06",
-        'j' => "R07",
-        'k' => "R08",
-        'l' => "R09",
-        'm' => "R10",
-        'n' => "RJa",
-        'o' => "RQu",
-        'p' => "RKi",
+        '0' => "B02",
+        '1' => "B03",
+        '2' => "B04",
+        '3' => "B05",
+        '4' => "B06",
+        '5' => "B07",
+        '6' => "B08",
+        '7' => "B09",
+        '8' => "B10",
+        '9' => "BJa",
+        'a' => "BQu",
+        'b' => "BKi",
+        'c' => "BAc",
+        'd' => "R02",
+        'e' => "R03",
+        'f' => "R04",
+        'g' => "R05",
+        'h' => "R06",
+        'i' => "R07",
+        'j' => "R08",
+        'k' => "R09",
+        'l' => "R10",
+        'm' => "RJa",
+        'n' => "RQu",
+        'o' => "RKi",
+        'p' => "RAc",
         _ => panic!("Unable to convert this number from a letter to a card code."),
     }
     .to_string()
