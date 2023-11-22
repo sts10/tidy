912a4894c03439e3a81bbc4da2659683c169a3ea
Sam Schlinkert <sschlinkert@gmail.com>
2023-11-22 12:43:35 -0500

    fixes mean edit distance bug by using f64 type throughout function

--- src/display_information/mod.rs
+++ src/display_information/mod.rs
@@ -357,8 +357,8 @@ fn find_shortest_edit_distance(list: &[String]) -> usize {
 
 /// Calculate the mean edit distance between all pairs of words on the list.
 pub fn find_mean_edit_distance(list: &[String]) -> f64 {
-    let mut sum_of_all_edit_distances = 0;
-    let mut number_of_edit_distances_measured = 0;
+    let mut sum_of_all_edit_distances: f64 = 0.0;
+    let mut number_of_edit_distances_measured: f64 = 0.0;
     for (i, word1) in list.iter().enumerate() {
         // The list[0..i] upper-bound in this inner loop is so that we don't do
         // twice as many calls as necessary. Otherwise we would be finding the
@@ -367,10 +367,14 @@ pub fn find_mean_edit_distance(list: &[String]) -> f64 {
         // distance to itself (0).
         for word2 in list[0..i].iter() {
             let this_edit_distance = find_edit_distance(word1, word2);
-            number_of_edit_distances_measured += 1;
-            sum_of_all_edit_distances += this_edit_distance as usize;
+            number_of_edit_distances_measured += 1.0;
+            sum_of_all_edit_distances += this_edit_distance as f64;
         }
     }
+    eprintln!(
+        "Number of edit distances recorded: {}\nSum of all edit distances: {}",
+        number_of_edit_distances_measured, sum_of_all_edit_distances
+    );
     (sum_of_all_edit_distances as f64) / (number_of_edit_distances_measured as f64)
 }
 
